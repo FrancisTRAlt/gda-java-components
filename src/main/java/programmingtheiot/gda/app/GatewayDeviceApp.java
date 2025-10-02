569612facd48625cf510148f9ddf9194c452b245
Francis Tran <xst-tran6832@stthomas.edu>
2025-10-02 14:27:23 -0500

    [Review] GatewayDeviceApp.java
    Added the code, but test came out nothing. No errors. https://github.com/programming-the-iot/book-exercise-tasks/issues/45

--- src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
+++ src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
@@ -21,6 +21,8 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import programmingtheiot.gda.system.SystemPerformanceManager;
+
 /**
  * Main GDA application.
  * 
@@ -38,6 +40,8 @@ public class GatewayDeviceApp
 	
 	private String configFile = ConfigConst.DEFAULT_CONFIG_FILE_NAME;
 
+	private SystemPerformanceManager sysPerfMgr = null;
+
 	// constructors
 	
 	/**
@@ -54,6 +58,12 @@ public class GatewayDeviceApp
 
 	public GatewayDeviceApp(String[] args)
 	{
+		super();
+	
+		_Logger.info("Initializing GDA...");
+		
+		this.sysPerfMgr = new SystemPerformanceManager();
+
 		parseArgs(args);
 	}
 	
@@ -152,9 +162,13 @@ public class GatewayDeviceApp
 		_Logger.info("Starting GDA...");
 		
 		try {
-			// TODO: Your code here
-			
-			_Logger.info("GDA started successfully.");
+			if (this.sysPerfMgr.startManager()) {
+				_Logger.info("GDA started successfully.");
+			} else {
+				_Logger.warning("Failed to start system performance manager!");
+				
+				stopApp(-1);
+			}
 		} catch (Exception e) {
 			_Logger.log(Level.SEVERE, "Failed to start GDA. Exiting.", e);
 			
@@ -172,9 +186,11 @@ public class GatewayDeviceApp
 		_Logger.info("Stopping GDA...");
 		
 		try {
-			// TODO: Your code here
-			
-			_Logger.log(Level.INFO, "GDA stopped successfully with exit code {0}.", code);
+			if (this.sysPerfMgr.stopManager()) {
+				_Logger.log(Level.INFO, "GDA stopped successfully with exit code {0}.", code);
+			} else {
+				_Logger.warning("Failed to stop system performance manager!");
+			}
 		} catch (Exception e) {
 			_Logger.log(Level.SEVERE, "Failed to cleanly stop GDA. Exiting.", e);
 		}
