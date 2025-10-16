b6b0ae600cd439a827f4408fcd1c500e8579311a
Francis Tran <xst-tran6832@stthomas.edu>
2025-10-16 15:12:31 -0500

    PIOT-GDA-05-004

--- src/main/java/programmingtheiot/gda/app/DeviceDataManager.java
+++ src/main/java/programmingtheiot/gda/app/DeviceDataManager.java
@@ -24,6 +24,7 @@ import programmingtheiot.data.ActuatorData;
 import programmingtheiot.data.DataUtil;
 import programmingtheiot.data.SensorData;
 import programmingtheiot.data.SystemPerformanceData;
+import programmingtheiot.data.SystemStateData;
 
 import programmingtheiot.gda.connection.CloudClientConnector;
 import programmingtheiot.gda.connection.CoapServerGateway;
@@ -33,6 +34,7 @@ import programmingtheiot.gda.connection.IRequestResponseClient;
 import programmingtheiot.gda.connection.MqttClientConnector;
 import programmingtheiot.gda.connection.RedisPersistenceAdapter;
 import programmingtheiot.gda.connection.SmtpClientConnector;
+import programmingtheiot.gda.system.SystemPerformanceManager;
 
 /**
  * Shell representation of class for student implementation.
@@ -50,15 +52,14 @@ public class DeviceDataManager implements IDataMessageListener
 	private boolean enableMqttClient = true;
 	private boolean enableCoapServer = false;
 	private boolean enableCloudClient = false;
-	private boolean enableSmtpClient = false;
 	private boolean enablePersistenceClient = false;
-	
-	private IActuatorDataListener actuatorDataListener = null;
+	private boolean enableSystemPerf = false;
+
 	private IPubSubClient mqttClient = null;
 	private IPubSubClient cloudClient = null;
 	private IPersistenceClient persistenceClient = null;
-	private IRequestResponseClient smtpClient = null;
 	private CoapServerGateway coapServer = null;
+	private SystemPerformanceManager sysPerfMgr = null;
 	
 	// constructors
 	
@@ -66,9 +67,27 @@ public class DeviceDataManager implements IDataMessageListener
 	{
 		super();
 		
-		initConnections();
+		ConfigUtil configUtil = ConfigUtil.getInstance();
+		
+		this.enableMqttClient =
+			configUtil.getBoolean(
+				ConfigConst.GATEWAY_DEVICE, ConfigConst.ENABLE_MQTT_CLIENT_KEY);
+		
+		this.enableCoapServer =
+			configUtil.getBoolean(
+				ConfigConst.GATEWAY_DEVICE, ConfigConst.ENABLE_COAP_SERVER_KEY);
+		
+		this.enableCloudClient =
+			configUtil.getBoolean(
+				ConfigConst.GATEWAY_DEVICE, ConfigConst.ENABLE_CLOUD_CLIENT_KEY);
+		
+		this.enablePersistenceClient =
+			configUtil.getBoolean(
+				ConfigConst.GATEWAY_DEVICE, ConfigConst.ENABLE_PERSISTENCE_CLIENT_KEY);
+		
+		initManager();
 	}
-	
+		
 	public DeviceDataManager(
 		boolean enableMqttClient,
 		boolean enableCoapClient,
@@ -87,31 +106,64 @@ public class DeviceDataManager implements IDataMessageListener
 	@Override
 	public boolean handleActuatorCommandResponse(ResourceNameEnum resourceName, ActuatorData data)
 	{
-		return false;
-	}
-
-	@Override
-	public boolean handleActuatorCommandRequest(ResourceNameEnum resourceName, ActuatorData data)
-	{
-		return false;
+		if (data != null) {
+			_Logger.info("Handling actuator response: " + data.getName());
+			
+			// this next call is optional for now
+			//this.handleIncomingDataAnalysis(resourceName, data);
+			
+			if (data.hasError()) {
+				_Logger.warning("Error flag set for ActuatorData instance.");
+			}
+			
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean handleIncomingMessage(ResourceNameEnum resourceName, String msg)
 	{
-		return false;
+		if (msg != null) {
+			_Logger.info("Handling incoming generic message: " + msg);
+			
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean handleSensorMessage(ResourceNameEnum resourceName, SensorData data)
 	{
-		return false;
+		if (data != null) {
+			_Logger.info("Handling sensor message: " + data.getName());
+			
+			if (data.hasError()) {
+				_Logger.warning("Error flag set for SensorData instance.");
+			}
+			
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean handleSystemPerformanceMessage(ResourceNameEnum resourceName, SystemPerformanceData data)
 	{
-		return false;
+		if (data != null) {
+			_Logger.info("Handling system performance message: " + data.getName());
+			
+			if (data.hasError()) {
+				_Logger.warning("Error flag set for SystemPerformanceData instance.");
+			}
+			
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	public void setActuatorDataListener(String name, IActuatorDataListener listener)
@@ -120,10 +172,16 @@ public class DeviceDataManager implements IDataMessageListener
 	
 	public void startManager()
 	{
+		if (this.sysPerfMgr != null) {
+			this.sysPerfMgr.startManager();
+		}
 	}
 	
 	public void stopManager()
 	{
+		if (this.sysPerfMgr != null) {
+			this.sysPerfMgr.stopManager();
+		}
 	}
 
 	
@@ -136,6 +194,35 @@ public class DeviceDataManager implements IDataMessageListener
 	 */
 	private void initConnections()
 	{
+	}
+
+	private void initManager()
+	{
+		ConfigUtil configUtil = ConfigUtil.getInstance();
+		
+		this.enableSystemPerf =
+			configUtil.getBoolean(ConfigConst.GATEWAY_DEVICE,  ConfigConst.ENABLE_SYSTEM_PERF_KEY);
+		
+		if (this.enableSystemPerf) {
+			this.sysPerfMgr = new SystemPerformanceManager();
+			this.sysPerfMgr.setDataMessageListener(this);
+		}
+		
+		if (this.enableMqttClient) {
+			// TODO: implement this in Lab Module 7
+		}
+		
+		if (this.enableCoapServer) {
+			// TODO: implement this in Lab Module 8
+		}
+		
+		if (this.enableCloudClient) {
+			// TODO: implement this in Lab Module 10
+		}
+		
+		if (this.enablePersistenceClient) {
+			// TODO: implement this as an optional exercise in Lab Module 5
+		}
 	}
 	
 }
