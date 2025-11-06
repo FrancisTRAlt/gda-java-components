f087fcadecc3b15b677e6e0e5fd735627514f2a9
Francis Tran <xst-tran6832@stthomas.edu>
2025-11-06 12:11:52 -0600

    PIOT-GDA-09-002 Semi

--- src/main/java/programmingtheiot/gda/connection/handlers/GenericCoapResourceHandler.java
+++ src/main/java/programmingtheiot/gda/connection/handlers/GenericCoapResourceHandler.java
@@ -22,6 +22,11 @@ import programmingtheiot.common.ConfigUtil;
 import programmingtheiot.common.IDataMessageListener;
 import programmingtheiot.common.ResourceNameEnum;
 
+import org.eclipse.californium.core.CoapHandler;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.coap.OptionSet;
+
+import programmingtheiot.common.IDataMessageListener;
 
 /**
  * Shell representation of class for student implementation.
@@ -34,6 +39,7 @@ public class GenericCoapResourceHandler extends CoapResource
 	private static final Logger _Logger =
 		Logger.getLogger(GenericCoapResourceHandler.class.getName());
 	
+	private IDataMessageListener dataMsgListener;
 	// params
 	
 	
@@ -84,6 +90,34 @@ public class GenericCoapResourceHandler extends CoapResource
 	
 	public void setDataMessageListener(IDataMessageListener listener)
 	{
+	}
+
+	@Override
+	public void onLoad(CoapResponse response)
+	{
+		if (response != null) {
+			OptionSet options = response.getOptions();
+			
+			_Logger.info("Processing CoAP response. Options: " + options);
+			_Logger.info("Processing CoAP response. MID: " + response.advanced().getMID());
+			_Logger.info("Processing CoAP response. Token: " + response.advanced().getTokenString());
+			_Logger.info("Processing CoAP response. Code: " + response.getCode());
+			
+			// TODO: parse payload and notify listener
+			_Logger.info(" --> Payload: " + response.getResponseText());
+			
+			if (this.dataMsgListener != null) {
+				// TODO: send listener the response
+			}
+		} else {
+			_Logger.warning("No CoAP response to process. Response is null.");
+		}
+	}
+
+	@Override
+	public void onError()
+	{
+		_Logger.warning("Error processing CoAP response. Ignoring.");
 	}
 	
 }
