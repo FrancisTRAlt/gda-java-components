9c712245b5df0e9b35283eb2f05f2e7ad113b193
Francis Tran <xst-tran6832@stthomas.edu>
2025-11-06 12:08:44 -0600

    PIOT-GDA-09-001 Semi

--- src/main/java/programmingtheiot/gda/connection/CoapClientConnector.java
+++ src/main/java/programmingtheiot/gda/connection/CoapClientConnector.java
@@ -11,21 +11,22 @@
 
 package programmingtheiot.gda.connection;
 
+import java.lang.module.Configuration;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.WebLink;
-import org.eclipse.californium.core.coap.CoAP.ResponseCode;
 import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.config.CoapConfig;
+import org.eclipse.californium.elements.config.UdpConfig;
 
 import programmingtheiot.common.ConfigConst;
 import programmingtheiot.common.ConfigUtil;
 import programmingtheiot.common.IDataMessageListener;
 import programmingtheiot.common.ResourceNameEnum;
-
-import programmingtheiot.data.DataUtil;
 
 /**
  * Shell representation of class for student implementation.
@@ -38,6 +39,12 @@ public class CoapClientConnector implements IRequestResponseClient
 	private static final Logger _Logger =
 		Logger.getLogger(CoapClientConnector.class.getName());
 	
+	private String     protocol;
+	private String     host;
+	private int        port;
+	private String     serverAddr;
+	private CoapClient clientConn;
+	private IDataMessageListener dataMsgListener;
 	// params
 	
 	
@@ -50,6 +57,24 @@ public class CoapClientConnector implements IRequestResponseClient
 	 */
 	public CoapClientConnector()
 	{
+		ConfigUtil config = ConfigUtil.getInstance();
+		this.host = config.getProperty(ConfigConst.COAP_GATEWAY_SERVICE, ConfigConst.HOST_KEY, ConfigConst.DEFAULT_HOST);
+
+		if (config.getBoolean(ConfigConst.COAP_GATEWAY_SERVICE, ConfigConst.ENABLE_CRYPT_KEY)) {
+			this.protocol = ConfigConst.DEFAULT_COAP_SECURE_PROTOCOL;
+			this.port     = config.getInteger(ConfigConst.COAP_GATEWAY_SERVICE, ConfigConst.SECURE_PORT_KEY, ConfigConst.DEFAULT_COAP_SECURE_PORT);
+		} else {
+			this.protocol = ConfigConst.DEFAULT_COAP_PROTOCOL;
+			this.port     = config.getInteger(ConfigConst.COAP_GATEWAY_SERVICE, ConfigConst.PORT_KEY, ConfigConst.DEFAULT_COAP_PORT);
+		}
+
+		// NOTE: URL does not have a protocol handler for "coap",
+		// so we need to construct the URL manually
+		this.serverAddr = this.protocol + "://" + this.host + ":" + this.port;
+
+		initClient();
+
+		_Logger.info("Using URL for server conn: " + this.serverAddr);
 	}
 		
 	/**
@@ -124,5 +149,31 @@ public class CoapClientConnector implements IRequestResponseClient
 
 	
 	// private methods
-	
+	private void initClient()
+	{
+		try {
+			// Create the CoAP configuration
+			//
+			// NOTE 1: You can create a custom configuration as well by setting
+			// NOTE 2: These settings are for UDP only. See RFC 8323 for more about CoAP/TCP.
+			// (https://datatracker.ietf.org/doc/html/rfc8323)
+			if (Configuration.getStandard() == null) {
+				Configuration config = new Configuration(
+					CoapConfig.DEFINITIONS, 
+					UdpConfig.DEFINITIONS
+				);
+
+				Configuration.setStandard(config);
+				_Logger.info("Initialized Californium configuration with standard definitions");
+			}
+			
+			// Create the CoAP client
+			this.clientConn = new CoapClient(this.serverAddr);
+			
+			_Logger.info("Created client connection to server / resource: " + this.serverAddr);
+
+		} catch (Exception e) {
+			_Logger.log(Level.SEVERE, "Failed to connect to server: " + getServerUri(), e);
+		}
+	}
 }
